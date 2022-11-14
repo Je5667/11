1a012261d6b0fc45b494952912ebe5ddaec7978a
Je5667 <113067745+Je5667@users.noreply.github.com>
2022-11-14 19:58:07 +0900

    05

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-int main(void){
-	int i;
-	int grade[5];
+int main(void)
+{
+	char *pc;
+	int *pi;
+	double *pd;
 	
-	for (i=0;i<5;i++){
-		printf("input value (%i): ", i);
-		scanf("%d", &grade[i]);
-	}
-
-	int *pointer = grade ;
-	int temp = 0;
-	for (i=0;i<5;i++){
-		printf("grade[%i] = %i \n", i, *(pointer + i));
-		temp += *(pointer + i);
-	}
-
-	printf("average: %i", temp/ 5);
-
+	pc = (char *)10000;
+	pi = (int*)10000;
+	pd = (double *)10000;
+	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
 	return 0;
 }
